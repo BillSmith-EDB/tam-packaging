859084d090bef115ee6d7f2c75c207670c11a886
BillSmith-EDB <bill.smith@enterprisedb.com>
2022-06-20 15:14:13 -0400

    Add --no-readline option to configure. Check for errors.

--- builds.edb/debian10_amd64.sh
+++ builds.edb/debian10_amd64.sh
@@ -4,18 +4,25 @@ cwd=`pwd`
 echo "Add code to build packages here. Currently in directory: $cwd"
 cd tam
 echo "BEGIN: Running configure step"
-./configure
+./configure --without-readline
+if [ $? -ne 0 ]; then
+    echo "ERROR: configure step failed. Exiting"
+fi      
 echo "END: Running configure step"
 
 echo "BEGIN: Running build step"
 make
+if [ $? -ne 0 ]; then
+    echo "ERROR: make step failed. Exiting"
+fi    
 echo "END: Running build step"
 
 echo "BEGIN: Running tests"
 make check
+if [ $? -ne 0 ]; then
+    echo "ERROR: make check step failed. Exiting"
+fi    
 echo "END: Running tests"
 
 mkdir debian
 touch debian/bogus_artifact
-
-
